022629fcd1e84bbd5bd2f0e4dca91aa918d42e19
paul blessing <paulblessing@gmail.com>
2023-08-09 10:45:19 +0100

    vue

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const app = Vue.createApp({
-    // data, funtions
-    template: 'h2'
+    // data, funtions to react to events that we want
+    // dont do this its gonna get messy template: '<h2> i am a template </h2>'
+
+    // conditional rendering
+    data() {
+        return {
+            showBooks: true,
+            title: 'The Final Empire',
+            author: ' Brandon Smaop',
+            age: 46
+        }
+    },
+    methods:{
+toggleShowbooks(showBooks){this.showBooks= !this.showBooks}
+    },
+    
 })
 
 app.mount('#app')
